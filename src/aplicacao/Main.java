ed38a40c71d80637ee52d60dd700688d385cd1e8
Barbara Geovana <barbara.geovanans@gmail.com>
2026-02-09 07:35:14 -0300

    feat: aprendendo a instanciar Data[Hora]

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -7,6 +7,7 @@ package aplicacao;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -14,13 +15,27 @@ import java.time.Instant;
  */
 public class Main {
     public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        
        LocalDate ld = LocalDate.now();
        LocalDateTime ldt = LocalDateTime.now();
        Instant ins = Instant.now();
-       LocalDate ldISO = LocalDate.parse("206-02-08");
+       
+       LocalDate ldISO = LocalDate.parse("2026-02-09");
+       LocalDateTime ldtISO = LocalDateTime.parse("2026-02-09T06:33:00");
+       Instant insISO = Instant.parse("2026-02-09T09:33:00Z"); 
+       Instant insISOSP = Instant.parse("2026-02-06T01:49:00-03:00"); 
+       
+       String text = ld.format(dtf);       
        
        System.out.println("LocalDate: "+ld);
        System.out.println("LocalDateTime: "+ldt);
        System.out.println("Instant: "+ins);
+       System.out.println("Data ISO8601: "+ldISO);       
+       System.out.println("Data Hora ISO8601: "+ldtISO);
+       System.out.println("Instant ISO8601: "+insISO);
+       System.out.println("Instant ISO8601: "+insISOSP);
+       System.out.println("Data formatada: "+text);
+       
     }
 }
